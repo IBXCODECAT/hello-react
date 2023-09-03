2163e8b69b02102347856e14ff173796074536cd
IBXCODECAT <47279783+IBXCODECAT@users.noreply.github.com>
2023-09-03 14:30:24 -0500

    First Functional Component

---
+++ src/components/Hello.js
@@ -0,0 +1,16 @@
+/*
+// Code for the Hello component using function syntax
+function Hello() {
+    //Return JSX
+    return <h1>Hello World!</h1>
+}
+*/
+
+// Code for the Hello component using arrow function syntax
+const Hello = () => {
+    //Return JSX
+    return <h1>Hello World!</h1>
+}
+
+//Exporting the component
+export default Hello;
